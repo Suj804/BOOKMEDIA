5a3a9382f0d4a1070e80311f76dc58ab21ec0a4d
Suj804 <chmcs2026.sujata.santra@gmail.com>
2026-03-13 09:41:31 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { CartProvider } from './CartContext/CartContext.jsx'
 import React from 'react'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
