6d5d4a1edc0540e828fd451639fd131890fb5562
Suj804 <chmcs2026.sujata.santra@gmail.com>
2026-03-15 15:19:51 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api=axios.create({
     //baseURL:'http://localhost:4000/books',
-    baseURL: 'https://bookmedia-2.onrender.com',
+    baseURL: 'https://bookmedia-ioi2.onrender.com',
 })
 export default api
